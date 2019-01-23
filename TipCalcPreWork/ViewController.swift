cf60127a2939815f44e6acc4757411b47810ed1f
Jashmae <jbaculpo@csumb.edu>
2019-01-23 15:01:08 -0800

    Initial Commit

---
+++ TipCalcPreWork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalcPreWork
+//
+//  Created by Jashmae Baculpo on 1/23/19.
+//  Copyright © 2019 Jashmae Baculpo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
