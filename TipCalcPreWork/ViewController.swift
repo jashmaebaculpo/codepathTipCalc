06606d6e72e1e4de954dc6908699a3a6697617bf
Jashmae <jbaculpo@csumb.edu>
2019-01-28 09:46:24 -0800

    fixed a small error where it wasn't giving me the amount of coins I needed

--- TipCalcPreWork/ViewController.swift
+++ TipCalcPreWork/ViewController.swift
@@ -23,6 +23,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var threePeepsLabel: UILabel!
     
     @IBOutlet weak var fourPeepsLabel: UILabel!
+    
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -37,18 +40,20 @@ class ViewController: UIViewController {
     @IBAction func calculateTIp(_ sender: Any) {
         let bill = Double(billFIeld.text!) ?? 0
         let tipPercentages = [0.15, 0.2, 0.25]
-        
+       
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
         let total = tip + bill
         
         
+        
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
-        let divide2 = total/2.00
+        
+        let divide2 = total/2
         twoPeepsLabel.text = String(format: "$%.2f", divide2)
-        let divide3 = total/3.00
+        let divide3 = total/3
         threePeepsLabel.text = String(format: "$%.2f", divide3)
-        let divide4 = total/4.00
+        let divide4 = total/4
         fourPeepsLabel.text = String(format: "$%.2f", divide4)
     }
 }
